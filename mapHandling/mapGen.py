e43101d3a71b1ca905251b30d33ba653171c8f91
Bálint László Bujdosó <bujdosobalint8@gmail.com>
2024-10-25 21:11:42 +0200

    kesz a mapgeneralas

--- mapHandling/mapGen.py
+++ mapHandling/mapGen.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.ndimage import gaussian_filter
 import os
 
@@ -8,6 +6,7 @@ instance_count = 0
 map_size = 0
 min_height, max_height = 0, 150
 map_counter = 1
+
 def get_valid_int_input(prompt, min_value=None, max_value=None):
     while True:
         try:
@@ -21,29 +20,36 @@ def get_valid_int_input(prompt, min_value=None, max_value=None):
 
 def initialize_inputs():
     global instance_count, map_size
-    instance_count = get_valid_int_input('Hány map generálódjon?', 1, None)
-    map_size = get_valid_int_input('Add meg a pályaméretet (min.: 10, max.: 100)', 10, 100)
-
+    instance_count = get_valid_int_input('Hány map generálódjon?: ', 1, None)
+    map_size = get_valid_int_input('Add meg a pályaméretet (min.: 10, max.: 100): ', 10, 100)
 
 def generate_maps():
+    global map_counter
     for i in range(instance_count):
-        map = np.random.randint(min_height,max_height+1,(map_size,map_size))
+        map_data = np.random.randint(min_height, max_height + 1, (map_size, map_size))
         sigma = 2
-        smoothed_map = gaussian_filter(map,sigma=sigma)
+        smoothed_map = gaussian_filter(map_data, sigma=sigma)
         write_into_file(smoothed_map)
-        map_counter+=1
+        map_counter += 1
+
 def write_into_file(smoothed_map):
-    file_name = f'/maps/map{map_counter}.txt'
-    with open(file_name,'w') as f:
+    global map_counter
+    file_name = f'maps/map{map_counter}.txt'
+    os.makedirs('maps', exist_ok=True)
+    with open(file_name, 'w') as f:
         max_heights = CheckMultipleMaxes(smoothed_map)
-        f.write(f'{", ".join(map(str,max_heights.astype(int)))}\n')
+        f.write(f'{", ".join(map(str, max_heights.astype(int)))}\n')
         for row in smoothed_map:
-            f.write(' '.join(map(str,row.astype(int))) + '\n')
+            f.write(' '.join(map(str, row.astype(int))) + '\n')
     print(f'Kész a(z) {map_counter}/{instance_count} map')
+
 def CheckMultipleMaxes(smoothed_map):
     flattened_map = smoothed_map.flatten()
     max_val = flattened_map.max()
     return flattened_map[flattened_map == max_val]
+
 initialize_inputs()
+generate_maps()
+
 
 
